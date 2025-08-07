b81a3b72d60e1f9acfb4e2999c5fd02f376aa901
Rakesh Chourasia <rakeshchourasia43@gmail.com>
2025-08-07 16:16:26 +0530

    Update invoice_generator.py

--- invoice_generator.py
+++ invoice_generator.py
@@ -113,7 +113,6 @@ with col1:
     stall_no = st.text_input("Stall Number")
 with col2:
     selected_date = st.date_input("Invoice Date", value=datetime.today())
-    date = selected_date.strftime("%d/%m/%Y")  # ✅ Ensures DD/MM/YYYY
     ph_no = st.text_input("Customer Phone No.")
     payment_method = st.selectbox("Payment Method", ["Cash", "UPI"])
 
